00b75fd31b2927b31bb5edfa41c2d60387109e87
Che-cop <jguymon2002@yahoo.com>
2016-05-05 22:45:12 +0000

    moved board variable into controller. Removed test spec. Added resonsiveness to game board. updated routes. rb games on;ly show. added broken test, neep help.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :players
   root :to => 'static_pages#index'
 
-  resources :games
+  resources :games, only: [:show]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
