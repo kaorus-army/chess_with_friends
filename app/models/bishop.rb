5af015847448037af13f0fcced9de0171dd99a0c
Nick Taylor <taylorn@udel.edu>
2016-05-12 12:54:37 -0400

    Wrote Bishop#valid_move? and specs

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -1,3 +1,15 @@
 class Bishop < Piece
-  # - can only move diagonally
+
+  def valid_move?(end_coord)
+    # Check basic move direction && coordinate
+    return false unless valid_move_direction?(end_coord) && game.valid_coordinate?(end_coord)
+    # Check direction is diagonal
+    direction = get_move_direction(end_coord)
+    return false unless direction == :diagonal
+    # Check obstruction
+    return false if obstructed?(end_coord)
+
+    true
+  end
+
 end
--- spec/models/bishop_spec.rb
+++ spec/models/bishop_spec.rb
@@ -5,4 +5,35 @@ RSpec.describe Bishop, type: :model do
     bishop = create(:bishop)
     expect(bishop).to be_valid
   end
+
+  describe "#valid_move?" do
+
+    let(:bishop) { create(:bishop, x_pos: 4, y_pos: 4) }
+
+    it "should be true for diagonal moves of any distance" do
+      expect(bishop.valid_move?([5, 5])).to eq true
+      expect(bishop.valid_move?([0, 0])).to eq true
+    end
+
+    it "should be false for horizontal moves" do
+      expect(bishop.valid_move?([7, 4])).to eq false
+    end
+
+    it "should be false for vertical moves" do
+      expect(bishop.valid_move?([4, 7])).to eq false
+    end
+
+    it "should be false for obstructed moves" do
+      game = create(:game)
+      obstructing_piece = create(:piece, x_pos: 3, y_pos: 2, game: game)
+      obstructed_bishop = create(:bishop, x_pos: 2, y_pos: 3, game: game)
+
+      expect(obstructed_bishop.valid_move?([4, 1])).to eq false
+    end
+
+    it "should be false for invalid coordinates" do
+      expect(bishop.valid_move?([8, 8])).to eq false
+    end
+
+  end # describe #valid_move?
 end
