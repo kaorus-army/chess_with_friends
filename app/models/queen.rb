20222c50629bef4f63fcf738c5fc62b9b4710c63
Che-cop <jguymon2002@yahoo.com>
2016-05-12 23:40:49 +0000

    added check direction to queen valid move

--- app/models/queen.rb
+++ app/models/queen.rb
@@ -2,7 +2,9 @@ class Queen < Piece
   # - can move in any direction for unlimited distance
   def valid_move?(end_coord)
     # Basic checks
-    return false unless game.valid_coordinate?(end_coord)
+    return false unless valid_move_direction?(end_coord) && game.valid_coordinate?(end_coord)
+    # Check direction
+    direction = get_move_direction(end_coord)
     # Check obstruction
     return false if obstructed?(end_coord)
 
