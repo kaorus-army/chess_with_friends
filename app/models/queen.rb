5029fbb02749bb58927195bd5b57974d7b31dfb2
Che-cop <jguymon2002@yahoo.com>
2016-05-12 19:28:30 +0000

    started queen move_vaild

--- app/models/queen.rb
+++ app/models/queen.rb
@@ -1,3 +1,12 @@
 class Queen < Piece
   # - can move in any direction for unlimited distance
+  def vaild_move?(end_coords)
+
+    return false unless game.valid_coordinate?(end_coord)
+
+    return false if obstructed?(end_coord)
+
+    true
+    
+  end
 end
