d3ac360e21afcc2f534195b5e30c52da0da00550
Che-cop <jguymon2002@yahoo.com>
2016-05-12 18:54:08 +0000

    added king vaild_move? with tests

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,4 +1,18 @@
 class King < Piece
   # - can only move one total space
   # - can move in any direction
+
+  def valid_move?(end_coord)
+
+    direction = get_move_direction(end_coord)
+    distance = distance_from(end_coord, direction)
+
+
+    if distance == 1
+      true
+    else
+      false
+    end
+  end
+
 end
--- spec/models/king_spec.rb
+++ spec/models/king_spec.rb
@@ -5,4 +5,25 @@ RSpec.describe King, type: :model do
     king = create(:king)
     expect(king).to be_valid
   end
+
+  describe "valid_move?" do
+
+    let(:king) { create(:king, x_pos:5, y_pos:5) }
+
+    it "should allow for only movement of one space " do
+      expect(king.valid_move?([4,5])).to eq true
+      expect(king.valid_move?([2,5])).to eq false
+    end
+
+    it "should allow king to move in any direction" do
+      expect(king.valid_move?([4,5])).to eq true #left
+      expect(king.valid_move?([6,5])).to eq true #right
+      expect(king.valid_move?([5,4])).to eq true #up
+      expect(king.valid_move?([5,6])).to eq true #down
+      expect(king.valid_move?([4,4])).to eq true #up-left
+      expect(king.valid_move?([6,4])).to eq true #up-left
+      expect(king.valid_move?([4,6])).to eq true #down-left
+      expect(king.valid_move?([6,6])).to eq true #down-right
+    end
+  end
 end
