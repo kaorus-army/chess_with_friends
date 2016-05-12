321422ed265ae023d7470437bc9590c9e2d5dec2
Che-cop <jguymon2002@yahoo.com>
2016-05-12 19:37:22 +0000

    added obstructed check and cleaned up code

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,20 +1,15 @@
 class King < Piece
-  # - can only move one total space
-  # - can move in any direction
 
   def valid_move?(end_coord)
 
     return false unless valid_move_direction?(end_coord) && game.vaild_coordinate?(end_coord)
-    
+    # Check distance
     direction = get_move_direction(end_coord)
     distance = distance_from(end_coord, direction)
+    return false if distance != 1
+    # Check obstruction
+    return false if obstructed?(end_coord)
 
-
-    if distance == 1
-      true
-    else
-      false
-    end
+    true
   end
-
 end
