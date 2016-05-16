90c77e7c2af5d2176b25ab20b5357e028272669a
Nick Taylor <taylorn@udel.edu>
2016-05-15 20:41:03 -0400

    Actually finished GameController#show specs

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,20 @@
 class GamesController < ApplicationController
+  before_action :authenticate_player!
+  before_action :set_game, only: [:show]
+  before_action :verify_game, only: [:show]
 
-def show
-  @board = [[0,1,0,1,0,1,0,1],[1,0,1,0,1,0,1,0]]
-end
+  def show
+    @board = [[0,1,0,1,0,1,0,1],[1,0,1,0,1,0,1,0]]
+  end
+
+  private
+
+  def set_game
+    @game = Game.find_by_id(params[:id])
+  end
+
+  def verify_game
+    render file: 'public/404', status: :not_found if set_game.blank?
+  end
 
 end
--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -4,18 +4,34 @@ RSpec.describe GamesController, type: :controller do
 
   context "when player signed in" do
 
-    describe "#show" do
+    before(:example) do
+      @player1 = create(:player)
+      sign_in(@player1)
+    end
 
-      before(:example) do
-        @player1 = create(:player)
-        sign_in(@player1)
-      end
+    describe "#show" do
 
       it "should successfully load the show page" do
         game = create(:game)
+        sign_out @player1
+        sign_in game.players.first
         get :show, id: game.id
 
         expect(response).to have_http_status(:success)
+      end
+
+      it "should 404 error if the game doesn't exist" do
+        get :show, id: 'TACO'
+
+        expect(response).to have_http_status(:not_found)
+      end
+
+      it "should restrict access to only players associated with the game" do
+        game = create(:game)
+        # Signed in as @player1
+        get :show, id: game.id
+
+        expect(response).to have_http_status(:forbidden)
       end
 
     end # describe #show
