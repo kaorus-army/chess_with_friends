b5cb81ba485a5ce6e89a5f6263e41b5436e5aa67
Nick Taylor <taylorn@udel.edu>
2016-05-15 20:44:38 -0400

    Repaired GameController#show specs

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,6 +2,7 @@ class GamesController < ApplicationController
   before_action :authenticate_player!
   before_action :set_game, only: [:show]
   before_action :verify_game, only: [:show]
+  before_action :authorize_game, only: [:show]
 
   def show
     @board = [[0,1,0,1,0,1,0,1],[1,0,1,0,1,0,1,0]]
@@ -15,6 +16,10 @@ class GamesController < ApplicationController
 
   def verify_game
     render file: 'public/404', status: :not_found if set_game.blank?
+  end
+
+  def authorize_game
+    render file: 'public/422', status: :forbidden unless @game.players.include?(current_player)
   end
 
 end
